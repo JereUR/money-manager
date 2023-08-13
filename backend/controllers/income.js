931894a8670a428d8b10f5b21715670d66e4be58
JereUR <jeremias.jdv@gmail.com>
2023-08-13 02:26:53 -0300

    feat(Root): Income schema created

---
+++ backend/controllers/income.js
@@ -0,0 +1,47 @@
+const IncomeSchema = require('../models/IncomeSchema')
+
+exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body
+
+  const income = IncomeSchema({ title, amount, category, description, date })
+
+  try {
+    //validations
+    if (!title || !amount || !category || !description || !date) {
+      return res
+        .status(400)
+        .json({ message: 'Todos los campos son requeridos.' })
+    }
+
+    if (amount <= 0 || !amount === 'number') {
+      return res
+        .status(400)
+        .json({ message: 'Monto debe ser un número positivo.' })
+    }
+
+    await income.save()
+    res.status(200).json({ message: 'Ingreso Agregado.' })
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor.' })
+  }
+}
+
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({ createdAt: -1 })
+    res.status(200).json({ incomes })
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor.' })
+  }
+}
+
+exports.deleteIncome = async (req, res) => {
+  const { id } = req.params
+  IncomeSchema.findByIdAndDelete(id)
+    .then((income) => {
+      res.status(200).json({ message: `Ingreso '${income.title}' Eliminado.` })
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Error en el servidor: ${error}` })
+    })
+}
