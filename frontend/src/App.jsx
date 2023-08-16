093c8d49dc25d5ccd58e54396b50e48e718786ff
JereUR <jeremias.jdv@gmail.com>
2023-08-15 21:04:15 -0300

    feat(Root): Navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components'
-import bg from './img/bg.png'
+import bg from './img/bg.jpg'
+import { MainLayout } from './styles/Layout'
+import Orb from './components/Orb/Orb'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
   return (
-    <AppStyled bg={bg}>
-      <main></main>
+    <AppStyled>
+      <Orb />
+      <MainLayout>
+        <Navigation />
+      </MainLayout>
     </AppStyled>
   )
 }
@@ -12,6 +18,8 @@ function App() {
 const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-size: cover;
   position: relative;
 `
 
