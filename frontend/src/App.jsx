64d9e4dcd9d8b3a4f2233d73df5a15ad1a8c7fd4
JereUR <jeremias.jdv@gmail.com>
2023-08-17 21:41:28 -0300

    feat(Root): Navigation with states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,42 @@
 import styled from 'styled-components'
+import { useMemo, useState } from 'react'
+
 import bg from './img/bg.jpg'
 import { MainLayout } from './styles/Layout'
 import Orb from './components/Orb/Orb'
 import Navigation from './components/Navigation/Navigation'
+import Dashboard from './components/Dashboard/Dashboard'
+import Incomes from './components/Incomes/Incomes'
+import Expenses from './components/Expenses/Expenses'
 
 function App() {
+  const [active, setActive] = useState(1)
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Incomes />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
   return (
     <AppStyled>
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   )
@@ -21,6 +48,20 @@ const AppStyled = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
+
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow: auto;
+    overflow-x: hidden;
+
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `
 
 export default App
