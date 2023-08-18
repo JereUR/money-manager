0bbb59834df303e4f9fc8f8cfda474ccf0a47293
JereUR <jeremias.jdv@gmail.com>
2023-08-17 21:42:12 -0300

    feat(Root): Components for diferents views/navigation choices

---
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import { InnerLayout } from '../../styles/Layout'
+
+export default function Dashboard() {
+  return (
+    <DashboardStyled>
+      <InnerLayout>DashBoard</InnerLayout>
+    </DashboardStyled>
+  )
+}
+
+const DashboardStyled = styled.div``
---
+++ frontend/src/components/Expenses/Expenses.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+import { InnerLayout } from '../../styles/Layout'
+
+export default function Expenses() {
+  return (
+    <ExpensesStyled>
+      <InnerLayout>Expenses</InnerLayout>
+    </ExpensesStyled>
+  )
+}
+
+const ExpensesStyled = styled.div``
