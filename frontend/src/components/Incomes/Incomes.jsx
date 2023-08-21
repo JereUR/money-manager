5023d3f43b991466372578a5de0e845aca2a1e54
JereUR <jeremias.jdv@gmail.com>
2023-08-20 21:10:46 -0300

    feat(Root): Show current incomes in db

--- frontend/src/components/Incomes/Incomes.jsx
+++ frontend/src/components/Incomes/Incomes.jsx
@@ -3,9 +3,15 @@ import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layout'
 import { useGlobalContext } from '../../context/globalContext'
 import Form from '../Form/Form'
+import { useEffect } from 'react'
+import IncomeItem from '../IncomeItem/IncomeItem'
 
 export default function Incomes() {
-  const { addIncome } = useGlobalContext()
+  const { addIncome, incomes, getIncomes } = useGlobalContext()
+
+  useEffect(() => {
+    getIncomes()
+  }, [incomes])
 
   return (
     <IncomesStyled>
@@ -15,11 +21,49 @@ export default function Incomes() {
           <div className="form-container">
             <Form />
           </div>
-          <div className="incomes"></div>
+          <div className="incomes">
+            {incomes.map((income) => {
+              const { _id, title, amount, date, category, description } = income
+
+              return (
+                <IncomeItem
+                  key={_id}
+                  id={_id}
+                  title={title}
+                  amount={amount}
+                  date={date}
+                  category={category}
+                  description={description}
+                  indicatorColor="var(--color-green)"
+                />
+              )
+            })}
+          </div>
         </div>
       </InnerLayout>
     </IncomesStyled>
   )
 }
 
-const IncomesStyled = styled.div``
+const IncomesStyled = styled.div`
+  display: flex;
+  overflow: auto;
+
+  .income-content {
+    display: flex;
+    gap: 2rem;
+
+    .incomes {
+      flex: 1;
+    }
+  }
+
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: var(--primary-color3);
+    border-radius: 5px;
+  }
+`
